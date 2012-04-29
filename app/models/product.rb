40ef9bd27cc2711bf7d2aa2afcf5ec6bca2a93a4
David Weisz <daweisz@gmail.com>
2012-04-29 23:40:36 +0300

    Store front index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  
+  # Validations
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|jpeg|png)$}i,
+    :message => 'must be a URL for GIF, JPG, JPEG or PNG image.'}  
 end
